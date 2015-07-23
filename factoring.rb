65fb4ace61b7d38d842d5c9e6594115be0771046
gianlucazigni <gianlucazigni@gmail.com>
2015-07-23 18:49:01 -0400

    Initial commit

---
+++ factoring.rb
@@ -0,0 +1,34 @@
+def factor(num)
+factors = Array.new
+x = 2
+	while num != 1
+		if num < 0
+			factors << -1
+			num *= -1
+		elsif num % x == 0
+			num /= x
+			factors << x
+			x += 1
+			x = 2
+		else
+			x += 1
+		end
+	end
+	
+	puts "Your Factors are:"
+	p factors
+	
+end
+
+input = 1
+
+while input != 0
+	puts "\nWhat number would you like to factor?"
+	puts "Enter 0 to exit."
+	input = gets.chomp.to_i
+	break if input == 0
+	factor(input)
+end
+
+
+
